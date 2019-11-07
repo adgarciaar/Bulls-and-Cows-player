5931ddb1ed39081c2d8f48b8c051066ed93760c3
Adrian Garcia <33211027+adgarciaar@users.noreply.github.com>
2019-11-07 14:07:50 -0500

    Add files via upload

--- BullsAndCows_H2.py
+++ BullsAndCows_H2.py
@@ -9,7 +9,7 @@ from BullsAndCowsBasePlayer import *
 class BullsAndCows_H0( BullsAndCowsBasePlayer ):
 
     ## -----------------------------------------------------------------------
-    m_parametro = None
+    m_CurrentSolution = None
 
     ## -----------------------------------------------------------------------
     def __init__( self, n, a ):
@@ -20,9 +20,28 @@ class BullsAndCows_H0( BullsAndCowsBasePlayer ):
     ## -----------------------------------------------------------------------
     def guess( self, b, c ):
         s = copy.deepcopy( list( self.m_Alphabet ) )
-        #random.shuffle( s )
-        #return ''.join( s[ 0: self.m_GuessSize ] )
-        # end def
+
+        if (b==0 and c==0):
+            for item in list( self.m_CurrentSolution ):
+                print("este es uno: "+s)
+                #quitar los caracteres que no sirvieron
+                self.m_Alphabet = self.m_Alphabet.replace( item, '' )
+            random.shuffle( self.m_Alphabet ) #con los caracteres restantes
+            self.m_CurrentSolution = ''.join( s[ 0: self.m_GuessSize ] )
+
+        elif(b == 0 and c > 0): #hay sólo picas
+            random.shuflle(self.m_CurrentSolution)
+
+        elif(b > and c == 0): #hay sólo fijas
+            pass
+
+        elif(b > 0 and c >0):
+            if( b+c == 4 ): #ya están todos los caracteres que se necesitan
+                pass #cambiar las posiciones
+
+
+        return self.m_CurrentSolution
+    # end def
 
 # end class
 
